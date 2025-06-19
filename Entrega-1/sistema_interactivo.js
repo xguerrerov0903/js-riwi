b204cca18188902c205e6c5877f1d717286b4e8e
xguerrerov0903 <xguerrerov0903@gmail.com>
2025-06-19 08:10:50 -0500

    Semana 1 terminada

--- Entrega-1/sistema_interactivo.js
+++ Entrega-1/sistema_interactivo.js
@@ -17,10 +17,11 @@ while (true){
     while (true){
         // Solicitud de la variable edad
         edad= prompt("Dime tu edad: ");
-        // Comprobacion de que se haya realizado un valor a la varaible edad 
-        if (!edad){
+        // Comprobacion de que se haya realizado un valor a la variable edad 
+        // Ademas de que la entreda sea unicamente numerica
+        if (!edad || !(/^-?\d+(.\d+)?$/.test(edad))){
             // De no haber un valor se entrega mensaje de error y se vuelve al inicio del ciclo que solicita la edad
-            alert("Error. No puedes dejar campos vacios");
+            alert("Error. No puedes dejar campos vacios o trataste de ingresar letras");
             continue;
         }
         break;
@@ -29,12 +30,12 @@ while (true){
     // Combierte a la variable edad en un int (si es que existen decimales los ignora y solo entrega el entero). En caso de que la cadena no sea numerica entrega NaN
     edad= parseInt(edad);
 
-    // Se comprueba tanto que el valor edad sea  un numero, como que sea un numero positivo
-    if (isNaN(edad) || edad<0){
+    // Se comprueba tanto que el valor edad sea  un numero (en el while de la entrada numerica ya se hace pero se deja ya que el ejercici se propuso originalmente con este condicional), como que sea un numero positivo
+    if (isNaN(Number(edad)) || edad<0){
         /*Aun que el ejercicio propone usar console.error no es la mejor opcion, el mensaje de error se ve en la consola saliendo de la estatica de los mensaje 
         que eran en un alert, el usuario no entenderia que error tiene a menos que consulte consola, por eso se opta por mejor usar un alert*/
         // console.error("Error. Entrada de edad invalidad, ingresa un valor numerico entero positivo");
-        alert("Error. Entrada de edad invalidad, ingresa un valor numerico entero positivo")
+        alert("Error. Entrada de edad invalidad, ingresa un valor numerico positivo")
     // En caso de que edad sea menor a 18 años se entrega el siguiente mensaje
     } else if(edad<18){
         alert("Hola "+nombre+", eres menor de edad. ¡Sigue aprendiendo y disfruta del codigo!");
