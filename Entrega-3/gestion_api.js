f9199e5935ef3820649fe72da6c08b160380d392
Alejandro <sr.alejandrorojas@gmail.com>
2025-07-02 11:09:26 -0500

    Entrega 3 terminada

--- Entrega-3/gestion_api.js
+++ Entrega-3/gestion_api.js
@@ -1,12 +1,15 @@
+// Objengo todos los productos y los imprimo
 fetch(`http://localhost:3000/productos`)
+    // Por defaut mi solicitud sera GET, caso que sucede aqui
+    // La data que optendra (response) sera traducida de su formato .json para js
     .then(response => response.json())
     .then(data => console.log("Productos disponibles:", data))
     .catch(error => console.error("Error al obtener los productos", error))
 
 const nuevoProducto = {"id": 4, "nombre": "Monitor", "precio": 200};
 
-// No deja de ejecutarse esta agregando infinitamente elproducto si no lo paro
-/*
+if (validarProducto(nuevoProducto)){
+// Agrega un nuevo objeto 
 fetch(`http://localhost:3000/productos`,{
     method: `POST`,
     headers: {"Content-Type":`application/json`},
@@ -15,24 +18,30 @@ fetch(`http://localhost:3000/productos`,{
     .then(response => response.json())
     .then(data => console.log("Productos agregado:", data))
     .catch(error => console.error("Error al agregar producto:", error))
-*/
+}
 
 const producotActualizado = {"nombre": "Laptop", "precio": 1400};
 
+if (validarProducto(producotActualizado)){
+// Modifico un objeto existente
 fetch(`http://localhost:3000/productos/1`,{
     method: `PUT`,
     headers: {"Content-Type":`application/json`},
+    // Transforma mi objeto productoActualizado en un JSON 
     body: JSON.stringify(producotActualizado)
 })
     .then(response => response.json())
     .then(data => console.log("Productos actualizado:", data))
     .catch(error => console.error("Error al actualizar producto:", error))
+} 
 
+// Elimina el objeto que corresponda al id 3
 fetch(`http://localhost:3000/productos/3`,{
     method: `DELETE`,})
-    .then(()=> console.log("Porducto eliminado", data))
+    .then(()=> console.log("Producto eliminado"))
     .catch(error => console.error("Error al eliminar producto:", error))
 
+// Valida que haya una entrada en nombre y que precio sea un valor numerico, de no serlo retorna false
 function validarProducto(producto){
     if (!producto.nombre || typeof producto.precio !== "number"){
         console.error("Datos del producto no validos");
